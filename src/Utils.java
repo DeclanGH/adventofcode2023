58da655de6d57bcd2628519533ddcfaba2a442dc
DeclanGH <declanoonunkwo@gmail.com>
2024-07-21 00:36:56 -0700

    create utils java class

---
+++ src/Utils.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Utils {
+
+    /**
+     * Retrieves all the string constants defined in the given class.
+     * Therefore, only static and final strings will be retrieved.
+     *
+     * @param myClass the class containing the string constants
+     * @return a set of string constants
+     */
+    public static Set<String> getStringConstantsInClassAsSet(Class<?> myClass) {
+        return Stream.of(myClass.getDeclaredFields())
+                .filter(field -> Modifier.isStatic(field.getModifiers()) &&
+                        Modifier.isFinal(field.getModifiers()) &&
+                        field.getType().equals(String.class))
+                .map(field -> {
+                    try {
+                        field.setAccessible(true);
+                        return (String) field.get(null);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+}
